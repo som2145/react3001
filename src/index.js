61cadd481d1eef7dc18932fdf7b1fc3c8275f3d7
박소민 <som2145@gmail.com>
2021-05-26 10:14:18 +0900

    2021.05.26

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Welcome(props){
-  return <h1>Hello, {props.name}</h1>
-}
-function App(){
-  return (
-    <div>
-      <Welcome name="park" />
-      <Welcome name="somin" />
-      <Welcome name="zzang" />
-    </div>
-  );
+function formatDate(date){
+  return date.toLocaleDateString();
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function Avatar(props){
+    return(
+      <img className="Avatar" src={props.user.avatarUrl} alt={props.user.name}/>
+    )
+  }
+
+function UserInfo(props){
+  return(
+    <div className="UserInfo">
+      <Avatar user={props.user}/>
+      <div>{props.user.name}</div>
+      <div>UserInfo</div>
+    </div>
+  )
+}
+function Comment(props){
+  return(
+    <div className="Comment">
+      <UserInfo user={props.author}/>
+      <div className="Comment-text">{props.text}</div>
+      <div className="Comment-text">{formatDate(props.date)}</div>
+    </div>
+  )
+}
+const comment ={
+  date: new Date(),
+  text: "열심히 할수록 기회는 따른다.",
+  author: {
+    name: "somin",
+    avatarUrl:"http://som2145.dothome.co.kr/portfolio/assets/imges/aboutImg01.jpg"
+  },
+};
+ReactDOM.render(<Comment date={comment.date} text={comment.text} author={comment.author}/>, document.getElementById("root"));
